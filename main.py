90186afed80b5da65c3289a3529fc26b3b7b110e
Ruan Carlos <ruancar500@gmail.com>
2026-03-31 16:06:14 -0300

    Refactor main.py to include user management features

--- main.py
+++ main.py
@@ -1,68 +1,99 @@
-from services.gerenciador import GerenciadorTarefas
-from database.db import criar_tabela
+from services.gerenciador_usuarios import GerenciadosUsuarios
+from services.gerenciador_tarefas import GerenciadorTarefas
+from database.db import criar_tabela_tarefas, criar_tabela_usuarios
 
-def menu():
+def menu_cadastro():
+    print("--- CADASTRO ---")
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu email: ")
+    senha = input("Digite sua senha: ")
+    return nome, email, senha
+
+
+def menu_login():
+    print(" --- LOGIN --- ")
+    email = input("Digite seu email: ")
+    senha = input("Digite sua senha: ")
+    return email, senha
+
+
+def menu_tarefas():
     print("""
-          --- GERENCIADOR ---
-          1. Adicionar
-          2. Listar
-          3. Concluir
-          4. Remover
-          5. Sair""")
+--- GERENCIADOR ---
+1. Adicionar
+2. Listar
+3. Concluir
+4. Remover
+5. Deslogar
+6. Excluir Conta
+""")
     
-def obter_id():
-    while True:
-        try:
-            t_id = int(input("ID: "))
-            return t_id
-        except ValueError:
-            print("❌ ID inválido. Por favor, insira um número válido")
 
 def main():
-    criar_tabela()
-    app = GerenciadorTarefas()
+    criar_tabela_usuarios()
+    criar_tabela_tarefas()
+
+    gerenciar_usuarios = GerenciadosUsuarios()
 
     while True:
-        menu()
+        escolha = input("Deseja 1. Logar ou 2. Cadastrar? [1/2]: ")
+        if escolha == "1":
+            email, senha = menu_login()
+            if gerenciar_usuarios.login(email, senha):
+                print("Login bem-sucedido!")
+                break
+            else:
+                print("Credenciais inválidas.")
+        elif escolha == "2":
+            nome, email, senha = menu_cadastro()
+            if gerenciar_usuarios.cadastrar_usuario(nome, email, senha):
+                print("Cadastro realizado com sucesso!")
+            else:
+                print("Erro ao cadastrar usuário.")
+        else:
+            print("Opção Inválida. Tente novamente.")
+
+    gerenciar_tarefas = GerenciadorTarefas(gerenciar_usuarios.obter_usuario_logado())
+
+    while True:
+        menu_tarefas()
         op = input("Escolha uma opção: ")
 
         if op == "1":
             desc = input("Descrição: ")
-            app.adicionar(desc)
-            print("✅ Tarefa adicionada encontrada")
+            print(gerenciar_tarefas.adicionar_tarefa(desc))
 
         elif op == "2":
-            tarefas = app.listar()
-            if not tarefas:
-                print("Nenhuma tarefa.")
+            tarefas = gerenciar_tarefas.listar_tarefas()
+            if isinstance(tarefas, str):
+                print(tarefas)
             else:
                 for tarefa in tarefas:
                     print(tarefa)
         
         elif op == "3":
-                t_id = obter_id()
-                result = app.concluir(t_id)
-
-                if result is True:
-                    print("✅ Tarefa Concluída!")
-                elif result is False:
-                    print("⚠ A tarefa já estava concluída!")
-                else:
-                    print("❌ Tarefa não encontrada.")
+                tarefa_id = int(input("ID da tarefa: "))
+                print(gerenciar_tarefas.concluir_tarefa(tarefa_id))
         
         elif op == "4":
-                t_id = obter_id()
-                if app.remover(t_id):
-                    print("🗑️ Tarefa Removida!")
-                else:
-                    print("❌ Tarefa não encontrado.")
-
+                tarefa_id = int(input("ID da tarefa: "))
+                print(gerenciar_tarefas.remover_tarefa(tarefa_id))
+        
         elif op == "5":
-            print("👋 Até logo!")
+            gerenciar_usuarios.logout()
+            print("Você foi deslogado com sucesso!")
+            break
+
+        elif op == "6":
+            confirmar = input("Tem certeza que deseja excluir sua conta? [S/N]").upper()
+            if confirmar == "S":
+                email = input("Digite seu email: ")
+                senha = input("Digite sua senha: ")
+                print(gerenciar_usuarios.excluir_conta(email, senha))
             break
             
         else:
-            print("Opção inválida! Por favor, escolha uma opção de 1 a 5.")
+            print("Opção inválida! Por favor, escolha uma opção de 1 a 6.")
 
 if __name__ == "__main__":
     main()
