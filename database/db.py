cf596ad65487ef8cf2c9c1b9d7d189b72d5a6c96
Ruan Carlos <ruancar500@gmail.com>
2026-03-31 16:02:30 -0300

    Refactor database connection and add usuarios table
    Updated database connection and created user table.

--- database/db.py
+++ database/db.py
@@ -1,17 +1,38 @@
 import sqlite3
 
-def conectar():
-    return sqlite3.connect("tarefas.db")
 
-def criar_tabela():
+def conectar():
+    conn = sqlite3.connect('gerenciador.db')
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
+
+def criar_tabela_usuarios():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios(
+                   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT NOT NULL,
+                   email TEXT NOT NULL,
+                   senha TEXT NOT NULL
+                   )
+                   """)
+    conn.commit()
+    conn.close()
+
+
+def criar_tabela_tarefas():
     conn = conectar()
     cursor = conn.cursor()
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS tarefas(
                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                    descricao TEXT NOT NULL,
-                   concluida INTEGER NOT NULL
+                   concluida INTEGER NOT NULL,
+                   usuario_id INTEGER,
+                   FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
                    )
                    """)
     conn.commit()
     conn.close()
+
